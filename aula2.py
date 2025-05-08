d0d315dbcbf7286570a5885c0a50df3745433550
xandi0253 <sistemasalomao@gmail.com>
2025-05-07 23:38:24 -0300

    Delete aula2.py

--- aula2.py
+++
@@ -1,6 +0,0 @@
-nome_aluno: str = "Alfa numerico" # variavel do tipo string, são textos
-idade_aluno: int = 15 # variavel do tipo inteiro, todos tipos de números inteiros
-ativo: bool = True # variavel do tipo booleano, verdadeiro ou falso - 1 ou 0
-nota_aluno: float = 8.75 # variavel do tipo float/decimal, números quebrados
-
-print(nome_aluno)
